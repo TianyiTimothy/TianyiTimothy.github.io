16ee104d1d6ec97ec8982837f480742d704d71df
TimothyZhang <TimothyZhang@foxmail.com>
2020-05-12 14:34:07 +0800

    Updates

---
+++ precache-manifest.476f1950c0ee584ac69ca0168a1364e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4060374346da3c344c6aa185f13281d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "369e34bc20c359c6df1c",
+    "url": "./static/css/main.74c310fd.chunk.css"
+  },
+  {
+    "revision": "0872ce7260b15fa66135",
+    "url": "./static/js/2.643f6b60.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "./static/js/2.643f6b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369e34bc20c359c6df1c",
+    "url": "./static/js/main.e164e43f.chunk.js"
+  },
+  {
+    "revision": "3e1b6ece78c02e0b3143",
+    "url": "./static/js/runtime-main.572f9a9b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "./static/media/image.d7265326.png"
+  }
+]);
